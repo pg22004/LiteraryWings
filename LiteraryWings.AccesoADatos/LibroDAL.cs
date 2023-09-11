140b7ea778cfb97a161fd41ca1b1dfccd00d5aa8
Axel León <lq22001@esfe.agape.edu.sv>
2023-09-11 16:25:11 -0600

    Codificación de la Entidad Libro y la DAL de Libro

--- LiteraryWings.AccesoADatos/LibroDAL.cs
+++ LiteraryWings.AccesoADatos/LibroDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteraryWings.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,159 @@ namespace LiteraryWings.AccesoADatos
 {
     public class LibroDAL
     {
+        public static async Task<int> CrearAsync(Libro pLibro)
+        {
+            int result = 0;
+            using (var bdcontexto = new DBContexto())
+            {
+                bdcontexto.Add(pLibro);
+                result = await bdcontexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Libro pLibro)
+        {
+            int result = 0;
+            using (var bdcontexto = new DBContexto())
+            {
+                var libro = await bdcontexto.Libro.FirstOrDefaultAsync(l => l.Id == pLibro.Id);
+                libro.Nombre = pLibro.Nombre;
+                libro.FechaLanzamiento = pLibro.FechaLanzamiento;
+                libro.IdAutor = pLibro.IdAutor;
+                libro.IdCategoria = pLibro.IdCategoria;
+                libro.IdEditorial = pLibro.IdEditorial;
+                libro.Idioma = pLibro.Idioma;
+                libro.Paginas = pLibro.Paginas;
+                libro.Descripcion = pLibro.Descripcion;
+                libro.ImagenPortada = pLibro.ImagenPortada;
+                libro.LinkDescarga = pLibro.LinkDescarga;
+                libro.ImagenIntroduccion = pLibro.ImagenIntroduccion;
+                bdcontexto.Update(libro);
+                result = await bdcontexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> EliminarAsync(Libro pLibro)
+        {
+            int result = 0;
+            using (var bdcontexto = new DBContexto())
+            {
+                var libro = await bdcontexto.Libro.FirstOrDefaultAsync(l => l.Id == pLibro.Id);
+                bdcontexto.Remove(libro);
+                result = await bdcontexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<Libro> ObtenerPorIdAsync(Libro pLibro)
+        {
+            var libro = new Libro();
+            using (var bdcontexto = new DBContexto())
+            {
+                libro = await bdcontexto.Libro.FirstOrDefaultAsync(l => l.Id == pLibro.Id);
+            }
+            return libro;
+        }
+
+        public static async Task<List<Libro>> ObtenerTodosAsync()
+        {
+            var libros = new List<Libro>();
+            using (var bdcontexto = new DBContexto())
+            {
+                libros = await bdcontexto.Libro.ToListAsync();
+            }
+            return libros;
+        }
+
+        internal static IQueryable<Libro> QuerySelect(IQueryable<Libro> pQuery, Libro pLibro)
+        {
+            if (pLibro.Id > 0)
+                pQuery = pQuery.Where(l => l.Id == pLibro.Id);
+
+            if (pLibro.IdAutor > 0)
+                pQuery = pQuery.Where(l => l.IdAutor == pLibro.IdAutor);
+
+            if (pLibro.IdCategoria > 0)
+                pQuery = pQuery.Where(l => l.IdCategoria == pLibro.IdCategoria);
+
+            if (pLibro.IdEditorial > 0)
+                pQuery = pQuery.Where(l => l.IdEditorial == pLibro.IdEditorial);
+
+            if (!string.IsNullOrWhiteSpace(pLibro.Nombre))
+                pQuery = pQuery.Where(l => l.Nombre.Contains(pLibro.Nombre));
+
+            if (pLibro.FechaLanzamiento.Year > 1000)
+            {
+                DateTime fechaInicial = new DateTime(pLibro.FechaLanzamiento.Year, pLibro.FechaLanzamiento.Month, pLibro.FechaLanzamiento.Day, 0, 0, 0);
+                DateTime fechaFinal = fechaInicial.AddDays(1).AddMilliseconds(-1);
+                pQuery = pQuery.Where(l => l.FechaLanzamiento >= fechaInicial && l.FechaLanzamiento <= fechaFinal);
+            }
+
+            if (!string.IsNullOrWhiteSpace(pLibro.Idioma))
+                pQuery = pQuery.Where(l => l.Idioma.Contains(pLibro.Idioma));
+
+            if (!string.IsNullOrWhiteSpace(pLibro.Descripcion))
+                pQuery = pQuery.Where(l => l.Descripcion.Contains(pLibro.Descripcion));
+
+            if (pLibro.Paginas > 0)
+                pQuery = pQuery.Where(l => l.Paginas == pLibro.Paginas);
+
+            pQuery = pQuery.OrderByDescending(l => l.Id).AsQueryable();
+            if (pLibro.top_aux > 0)
+                pQuery = pQuery.Take(pLibro.top_aux).AsQueryable();
+
+            return pQuery;
+        }
+
+        public static async Task<List<Libro>> BuscarAsync(Libro pLibro)
+        {
+            var libros = new List<Libro>();
+            using (var bdcontexto = new DBContexto())
+            {
+                var select = bdcontexto.Libro.AsQueryable();
+                select = QuerySelect(select, pLibro);
+                libros = await select.ToListAsync();
+            }
+            return libros;
+        }
+
+
+        public static async Task<List<Libro>> BuscarIncluirAutorAsync(Libro pLibro)
+        {
+            var libros = new List<Libro>();
+            using (var bdcontexto = new DBContexto())
+            {
+                var select = bdcontexto.Libro.AsQueryable();
+                select = QuerySelect(select, pLibro).Include(l => l.Autor).AsQueryable();
+                libros = await select.ToListAsync();
+            }
+            return libros;
+        }
+
+        public static async Task<List<Libro>> BuscarIncluirCategoriaAsync(Libro pLibro)
+        {
+            var libros = new List<Libro>();
+            using (var bdcontexto = new DBContexto())
+            {
+                var select = bdcontexto.Libro.AsQueryable();
+                select = QuerySelect(select, pLibro).Include(l => l.Categoria).AsQueryable();
+                libros = await select.ToListAsync();
+            }
+            return libros;
+        }
+
+        public static async Task<List<Libro>> BuscarIncluirEditorialAsync(Libro pLibro)
+        {
+            var libros = new List<Libro>();
+            using (var bdcontexto = new DBContexto())
+            {
+                var select = bdcontexto.Libro.AsQueryable();
+                select = QuerySelect(select, pLibro).Include(l => l.Editorial).AsQueryable();
+                libros = await select.ToListAsync();
+            }
+            return libros;
+        }
     }
 }
--- LiteraryWings.EntidadesDeNegocio/Libro.cs
+++ LiteraryWings.EntidadesDeNegocio/Libro.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LiteraryWings.EntidadesDeNegocio
 {
     public class Libro
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Autor")]
+        [Required(ErrorMessage = "El Autor es obligatorio")]
+        [Display(Name = "Autor")]
+        public int IdAutor { get; set; }
+
+        [ForeignKey("Categoria")]
+        [Required(ErrorMessage = "La Categoria es obligatoria")]
+        [Display(Name = "Categoria")]
+        public int IdCategoria { get; set; }
+
+        [ForeignKey("Editorial")]
+        [Required(ErrorMessage = "El Editorial es obligatorio")]
+        [Display(Name = "Editorial")]
+        public int IdEditorial { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La Fecha de Lanzamiento es obligatoria")]
+        [Display(Name = "Fecha de Lanzamiento ")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "El Idioma es obligatorio")]
+        [MaxLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        public string Idioma { get; set; }
+
+        [Required(ErrorMessage = "Las Paginas es obligatoria")]
+        public int Paginas { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es obligatoria")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La Imagen de Portada es obligatoria")]
+        [Display(Name = "Imagen de Portada")]
+        public string ImagenPortada { get; set; }
+
+        [Required(ErrorMessage = "El Link de Descarga es obligatorio")]
+        [Display(Name = "Link de Descarga")]
+        public string LinkDescarga { get; set; }
+
+        [Required(ErrorMessage = "La Imagen de Introduccion es obligatoria")]
+        [Display(Name = "Imagen de Introduccion")]
+        public string ImagenIntroduccion { get; set; }
+
+
+        //propiedad de navegación
+        public Autor Autor { get; set; }
+        public Categoria Categoria { get; set; }
+        public Editorial Editorial { get; set; }
+
+        //propiedades auxiliares
+        [NotMapped]
+        public int top_aux { get; set; }
+
     }
 }
