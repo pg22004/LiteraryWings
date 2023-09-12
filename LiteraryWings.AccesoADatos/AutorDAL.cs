de84d716809a030054eacc35dcec91f297452932
NestorEstupinian <em22001@esfe.edu.agape.edu.sv>
2023-09-11 21:52:23 -0600

    Codificacion de la DAL de Autor y Rol

--- LiteraryWings.AccesoADatos/AutorDAL.cs
+++ LiteraryWings.AccesoADatos/AutorDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteraryWings.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,99 @@ namespace LiteraryWings.AccesoADatos
 {
     public class AutorDAL
     {
+        public static async Task<int> CrearAsync(Autor pAutor)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                dbContexto.Add(pAutor);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Autor pAutor)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var autor = await dbContexto.Autor.FirstOrDefaultAsync(a => a.Id == pAutor.Id);
+                autor.Nombre = pAutor.Nombre;
+                autor.Apellido = pAutor.Apellido;
+                autor.FechaNacimiento = pAutor.FechaNacimiento;
+                autor.AutorImagen = pAutor.AutorImagen;
+                dbContexto.Update(autor);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> EliminarAsync(Autor pAutor)
+        {
+            int result = 0;
+            using (var dbContexto = new DBContexto())
+            {
+                var autor = await dbContexto.Autor.FirstOrDefaultAsync(a => a.Id == pAutor.Id);
+                dbContexto.Autor.Remove(autor);
+                result = await dbContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<Autor> ObtenerPorIdAsync(Autor pAutor)
+        {
+            var autor = new Autor();
+            using (var dbContexto = new DBContexto())
+            {
+                autor = await dbContexto.Autor.FirstOrDefaultAsync(a => a.Id == pAutor.Id);
+            }
+            return autor;
+        }
+
+        public static async Task<List<Autor>> ObtenerTodosAsync()
+        {
+            var autores = new List<Autor>();
+            using (var dbContexto = new DBContexto())
+            {
+                autores = await dbContexto.Autor.ToListAsync();
+            }
+            return autores;
+        }
+
+        internal static IQueryable<Autor> QuerySelect(IQueryable<Autor> pQuery, Autor pAutor)
+        {
+            if (pAutor.Id > 0)
+                pQuery = pQuery.Where(a => a.Id == pAutor.Id);
+
+            if (!string.IsNullOrWhiteSpace(pAutor.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pAutor.Nombre));
+
+            if (!string.IsNullOrWhiteSpace(pAutor.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pAutor.Nombre));
+
+            if (pAutor.FechaNacimiento.Year > 1000)
+            {
+                DateTime fechaInicial = new DateTime(pAutor.FechaNacimiento.Year, pAutor.FechaNacimiento.Month, pAutor.FechaNacimiento.Day, 0, 0, 0);
+                DateTime fechaFinal = fechaInicial.AddDays(1).AddMilliseconds(-1);
+                pQuery = pQuery.Where(a => a.FechaNacimiento >= fechaInicial && a.FechaNacimiento <= fechaFinal);
+            }
+
+            if (pAutor.Top_Aux > 0)
+                pQuery = pQuery.Take(pAutor.Top_Aux).AsQueryable();
+
+            return pQuery;
+        }
+
+        public static async Task<List<Autor>> BuscarAsync(Autor pAutor)
+        {
+            var autores = new List<Autor>();
+            using (var dbContexto = new DBContexto())
+            {
+                var select = dbContexto.Autor.AsQueryable();
+                select = QuerySelect(select, pAutor);
+                autores = await select.ToListAsync();
+            }
+            return autores;
+        }
     }
 }
