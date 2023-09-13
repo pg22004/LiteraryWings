0bce7c96026d09ba23486e1ae28e64e3d6eb9070
Iliana <pg22004@esfe.agape.edu.sv>
2023-09-13 02:05:24 +0200

    Codificacion de clases CategoriaBL y UsuarioBL

--- LiteraryWings.LogicaDeNegocio/CategoriaBL.cs
+++ LiteraryWings.LogicaDeNegocio/CategoriaBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteraryWings.EntidadesDeNegocio;
+using LiteraryWings.AccesoADatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace LiteraryWings.LogicaDeNegocio
 {
     public class CategoriaBL
     {
+        public async Task<int> CrearAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.CrearAsync(pCategoria);
+        }
+
+        public async Task<int> ModificarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ModificarAsync(pCategoria);
+        }
+
+        public async Task<int> EliminarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.EliminarAsync(pCategoria);
+        }
+
+        public async Task<Categoria> ObtenerPorIdAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ObtenerPorIdAsync(pCategoria);
+        }
+
+        public async Task<List<Categoria>> ObtenerTodosAsync()
+        {
+            return await CategoriaDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Categoria>> BuscarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.BuscarAsync(pCategoria);
+        }
     }
 }
