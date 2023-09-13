8b8516fb71a29888787f15a97ac18ee1de6cbdb5
SERRANOGRIS <ss22001@esfe.agape.edu.sv>
2023-09-13 16:29:11 -0300

    Codificacion de la clase EditorialBL

--- LiteraryWings.LogicaDeNegocio/EditorialBL.cs
+++ LiteraryWings.LogicaDeNegocio/EditorialBL.cs
@@ -4,9 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using LiteraryWings.AccesoADatos;
+using LiteraryWings.EntidadesDeNegocio;
+
 namespace LiteraryWings.LogicaDeNegocio
 {
-    public class EditorialBL
-    {
+   
+        public class EditorialBL
+        {
+            public async Task<int> CrearAsync(Editorial pEditorial)
+            {
+                return await EditorialDAL.CrearAsync(pEditorial);
+            }
+
+            public async Task<int> ModificarAsync(Editorial pEditorial)
+            {
+                return await EditorialDAL.ModificarAsync(pEditorial);
+            }
+
+            public async Task<int> EliminarAsync(Editorial pEditorial)
+            {
+                return await EditorialDAL.EliminarAsync(pEditorial);
+            }
+
+            public async Task<Editorial> ObtenerPorIdAsync(Editorial pEditorial)
+            {
+                return await EditorialDAL.ObtenerPorIdAsync(pEditorial);
+            }
+
+            public async Task<List<Editorial>> ObtenerTodosAsync()
+            {
+                return await EditorialDAL.ObtenerTodosAsync();
+            }
+
+            public async Task<List<Editorial>> BuscarAsync(Editorial pEditorial)
+            {
+                return await EditorialDAL.BuscarAsync(pEditorial);
+            }
+        }
     }
-}
+
