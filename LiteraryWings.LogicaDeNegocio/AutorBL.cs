05c5d1a58f5ffb0fffeb4f7dfec63f859ff78c73
NestorEstupinian <em22001@esfe.edu.agape.edu.sv>
2023-09-13 14:58:24 -0600

    Codificacion de la logica de negocio de Autor y Rol

--- LiteraryWings.LogicaDeNegocio/AutorBL.cs
+++ LiteraryWings.LogicaDeNegocio/AutorBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteraryWings.AccesoADatos;
+using LiteraryWings.EntidadesDeNegocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace LiteraryWings.LogicaDeNegocio
 {
     public class AutorBL
     {
+        public async Task<int> CrearAsync(Autor pAtor)
+        {
+            return await AutorDAL.CrearAsync(pAtor);
+        }
+
+        public async Task<int> ModificarAsync(Autor pAtor)
+        {
+            return await AutorDAL.ModificarAsync(pAtor);
+        }
+
+        public async Task<int> EliminarAsync(Autor pAtor)
+        {
+            return await AutorDAL.EliminarAsync(pAtor);
+        }
+
+        public async Task<Autor> ObtenerPorIdAsync(Autor pAtor)
+        {
+            return await AutorDAL.ObtenerPorIdAsync(pAtor);
+        }
+
+        public async Task<List<Autor>> ObtenerTodosAsync()
+        {
+            return await AutorDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Autor>> BuscarAsync(Autor pAtor)
+        {
+            return await AutorDAL.BuscarAsync(pAtor);
+        }
     }
 }
--- LiteraryWings.LogicaDeNegocio/RolBL.cs
+++ LiteraryWings.LogicaDeNegocio/RolBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiteraryWings.AccesoADatos;
+using LiteraryWings.EntidadesDeNegocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace LiteraryWings.LogicaDeNegocio
 {
     public class RolBL
     {
+        public async Task<int> CrearAsync(Rol pRol)
+        {
+            return await RolDAL.CrearAsync(pRol);
+        }
+
+        public async Task<int> ModificarAsync(Rol pRol)
+        {
+            return await RolDAL.ModificarAsync(pRol);
+        }
+
+        public async Task<int> EliminarAsync(Rol pRol)
+        {
+            return await RolDAL.EliminarAsync(pRol);
+        }
+
+        public async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        {
+            return await RolDAL.ObtenerPorIdAsync(pRol);
+        }
+
+        public async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            return await RolDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            return await RolDAL.BuscarAsync(pRol);
+        }
     }
 }
