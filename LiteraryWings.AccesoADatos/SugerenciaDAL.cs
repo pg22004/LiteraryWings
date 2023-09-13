f9f77f9822b26ed482bba90938e6c15249cf1adc
Yanira <oc22002@esfe.agape.edu.sv>
2023-09-12 15:54:15 -1200

    Codificación de las entidades Sugerencia y SugerenciaDAL

--- LiteraryWings.AccesoADatos/SugerenciaDAL.cs
+++ LiteraryWings.AccesoADatos/SugerenciaDAL.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LiteraryWings.EntidadesDeNegocio;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,95 @@ namespace LiteraryWings.AccesoADatos
 {
     public class SugerenciaDAL
     {
+            public static async Task<int> CrearAsync(Sugerencia pSugerencia)
+            {
+                int result = 0;
+                using (var bdContexto = new DBContexto())
+                {
+                    bdContexto.Add(pSugerencia);
+                    result = await bdContexto.SaveChangesAsync();
+                }
+                return result;
+            }
+
+            public static async Task<int> ModificarAsync(Sugerencia pSugerencia)
+            {
+                int result = 0;
+                using (var bdContexto = new DBContexto())
+                {
+                    var sugerencia = await bdContexto.Sugerencia.FirstOrDefaultAsync(s => s.Id == pSugerencia.Id);
+                    sugerencia.Nombre = pSugerencia.Nombre;
+                    bdContexto.Update(sugerencia);
+                    result = await bdContexto.SaveChangesAsync();
+                }
+                return result;
+            }
+
+            public static async Task<int> EliminarAsync(Sugerencia pSugerencia)
+            {
+                int result = 0;
+                using (var bdContexto = new DBContexto())
+                {
+                    var sugerencia = await bdContexto.Sugerencia.FirstOrDefaultAsync(s => s.Id == pSugerencia.Id);
+                    bdContexto.Sugerencia.Remove(sugerencia);
+                    result = await bdContexto.SaveChangesAsync();
+                }
+                return result;
+            }
+
+            public static async Task<Sugerencia> ObtenerPorIdAsync(Sugerencia pSugerencia)
+            {
+                var sugerencia = new Sugerencia();
+                using (var bdContexto = new DBContexto())
+                {
+                    sugerencia = await bdContexto.Sugerencia.FirstOrDefaultAsync(s => s.Id == pSugerencia.Id);
+                }
+                return sugerencia;
+            }
+
+            public static async Task<List<Sugerencia>> ObtenerTodosAsync()
+            {
+                var sugerencias = new List<Sugerencia>();
+                using (var bdContexto = new DBContexto())
+                {
+                    sugerencias = await bdContexto.Sugerencia.ToListAsync();
+                }
+                return sugerencias;
+            }
+
+            internal static IQueryable<Sugerencia> QuerySelect(IQueryable<Sugerencia> pQuery, Sugerencia pSugerencia)
+            {
+                if (pSugerencia.Id > 0)
+                    pQuery = pQuery.Where(s => s.Id == pSugerencia.Id);
+
+                if (!string.IsNullOrWhiteSpace(pSugerencia.Nombre))
+                    pQuery = pQuery.Where(s => s.Nombre.Contains(pSugerencia.Nombre));
+
+                pQuery = pQuery.OrderByDescending(s => s.Id).AsQueryable();
+
+                if (pSugerencia.Top_Aux > 0)
+                    pQuery = pQuery.Take(pSugerencia.Top_Aux).AsQueryable();
+
+                return pQuery;
+            }
+
+            public static async Task<List<Sugerencia>> BuscarAsync(Sugerencia pSugerencia)
+            {
+                var sugerencias = new List<Sugerencia>();
+                using (var bdContexto = new DBContexto())
+                {
+                    var select = bdContexto.Sugerencia.AsQueryable();
+                    select = QuerySelect(select, pSugerencia);
+                    sugerencias = await select.ToListAsync();
+                }
+                return sugerencias;
+            }
+        }
     }
-}
+
+
+
+
+
+
+
--- LiteraryWings.EntidadesDeNegocio/Sugerencia.cs
+++ LiteraryWings.EntidadesDeNegocio/Sugerencia.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace LiteraryWings.EntidadesDeNegocio
 {
     public class Sugerencia
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El largo máximo es de 30 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [MaxLength(35, ErrorMessage = "El largo máximo es de 35 caracteres")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El comentario es obligatoria")]
+        public string Comentario { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
     }
 }
