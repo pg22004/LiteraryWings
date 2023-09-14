b87b282beee6d646b749eeb89b3a9904475a6a4f
NestorEstupinian <em22001@esfe.edu.agape.edu.sv>
2023-09-13 21:51:21 -0600

    Codificacion de los controladores de Autor y Rol

--- LiteraryWings.WebAPI/Controllers/AutorController.cs
+++ LiteraryWings.WebAPI/Controllers/AutorController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LiteraryWings.EntidadesDeNegocio;
+using LiteraryWings.LogicaDeNegocio;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace LiteraryWings.WebAPI.Controllers
 {
@@ -7,5 +11,78 @@ namespace LiteraryWings.WebAPI.Controllers
     [ApiController]
     public class AutorController : ControllerBase
     {
+        private AutorBL autorBL = new AutorBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            return await autorBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Autor> Get(int id)
+        {
+            Autor autor = new Autor();
+            autor.Id = id;
+            return await autorBL.ObtenerPorIdAsync(autor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            try
+            {
+                await autorBL.CrearAsync(autor);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Autor autor)
+        {
+
+            if (autor.Id == id)
+            {
+                await autorBL.ModificarAsync(autor);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Autor autor = new Autor();
+                autor.Id = id;
+                await autorBL.EliminarAsync(autor);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Autor>> Buscar([FromBody] object pAutor)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strAutor = JsonSerializer.Serialize(pAutor);
+            Autor autor = JsonSerializer.Deserialize<Autor>(strAutor, option);
+            return await autorBL.BuscarAsync(autor);
+
+        }
     }
 }
