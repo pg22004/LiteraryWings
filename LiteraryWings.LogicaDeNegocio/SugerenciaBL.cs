9e1380cf87fdffd69e21d4da17d913a3d3b4ec46
Yanira <oc22002@esfe.agape.edu.sv>
2023-09-13 14:36:19 -1200

    Codificacion de la clase Sugerencia BL

--- LiteraryWings.LogicaDeNegocio/SugerenciaBL.cs
+++ LiteraryWings.LogicaDeNegocio/SugerenciaBL.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiteraryWings.AccesoADatos;
+using LiteraryWings.EntidadesDeNegocio;
 
 namespace LiteraryWings.LogicaDeNegocio
 {
     public class SugerenciaBL
     {
+        public async Task<int> CrearAsync(Sugerencia pSugerencia)
+        {
+            return await SugerenciaDAL.CrearAsync(pSugerencia);
+        }
+
+        public async Task<int> ModificarAsync(Sugerencia pSugerencia)
+        {
+            return await SugerenciaDAL.ModificarAsync(pSugerencia);
+        }
+
+        public async Task<int> EliminarAsync(Sugerencia pSugerencia)
+        {
+            return await SugerenciaDAL.EliminarAsync(pSugerencia);
+        }
+
+        public async Task<Sugerencia> ObtenerPorIdAsync(Sugerencia pSugerencia)
+        {
+            return await SugerenciaDAL.ObtenerPorIdAsync(pSugerencia);
+        }
+
+        public async Task<List<Sugerencia>> ObtenerTodosAsync()
+        {
+            return await SugerenciaDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Sugerencia>> BuscarAsync(Sugerencia pSugerencia)
+        {
+            return await SugerenciaDAL.BuscarAsync(pSugerencia);
+        }
     }
 }
